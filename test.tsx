16ebd1d06d07336e8b05b7a6fffce38c5a8f51fd
Đình Tiến <66708837+dinhtienps10163@users.noreply.github.com>
2020-08-27 12:34:11 +0700

    update component

--- test.tsx
+++ test.tsx
@@ -9,31 +9,29 @@ import Gradient from 'react-native-linear-gradient';
 import ItemHotelAroundHere from './src/city/ItemHotelAroundHere';
 import ButtonCustom from './src/components/custombutton';
 import configureStore from './src/redux/store';
+import CustomCirle from './src/components/CustomCircle';
+import ItemUser from './src/notification/ItemUser';
+import ItemNotification from './src/notification/ItemNotification'
 import {heightPercentageToDP as hp, widthPercentageToDP as wp} from 'react-native-responsive-screen';
 
 class index extends Component<any, any> {
   render() {
     return (
 <Provider store={configureStore}>
-        <View style={{flex: 1}}>
-        <View style={{marginTop: 50, position:'relative'}}>
-          
-        <ItemImage
-            source={require('./src/assets/images/lai_chau.jpg')}
-            isShow = {true}
-            title="da nang"
+        <ItemNotification
+        source={require('./src/assets/images/images.jpg')}
+        title='đáfgalkjasfklasjflks đaf asd asda á aj'
+        calendar='12/2/2000'
+        time='11h30'
+        day='21/12/2020'
+        isShow={true}
         />
-      <ItemHotelAroundHere
-        source={require('./src/assets/images/congtamquan.jpg')}
-        title="Title"
-        content="content"
-        titlePrice="100"
-        titleUSD="USD"
-        starCount="3"
-      />
-      
-      </View>        
-        </View>
+        <ItemUser
+        source={require('./src/assets/images/images.jpg')}
+        name='áddấdasdasds'
+        core='123'
+        />
+        
       </Provider>
 
       
