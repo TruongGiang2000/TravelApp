15cda6312893d4cb9cccc11c771c5b8cd1fd125e
Giang <truonggiang20001807@gmail.com>
2020-09-21 14:25:10 +0700

    Update SlideShow

---
+++ src/components/HomeSlideShow.tsx
@@ -0,0 +1,94 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, Text, ImageBackground, TextInput} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import ButtonCustom from './ButtonCustom';
+import shadow from './shadow';
+import GradientOpacity from './GradientOpacity';
+class HomeSlideShow extends Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const {style, data} = this.props;
+    return (
+      <ImageBackground
+        resizeMode={'cover'}
+        source={data.srcImage}
+        style={[styles.MainContainer, style]}>
+        <View style={styles.searchBarView}>
+          <MaterialIcons
+            name={'location-on'}
+            color={'#353B50'}
+            size={wp('6')}
+          />
+          <TextInput
+            placeholder={'Search'}
+            placeholderTextColor={'#A8B6C8'}
+            style={styles.textInputStyle}
+          />
+        </View>
+
+        <View style={styles.viewBottom}>
+          <GradientOpacity
+            start={{x: 0.1, y: 0.1}}
+            end={{x: 0.1, y: 0.1}}
+            styleGradient={styles.gradientOpacity}
+          />
+          <Text style={styles.titleStyle}>{data.title}</Text>
+          <Text style={styles.content}>{data.content}</Text>
+          <ButtonCustom style={styles.buttonStyle} title={'discover'} />
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  MainContainer: {
+    paddingVertical: hp('4'),
+  },
+  searchBarView: {
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    width: wp('90'),
+    borderRadius: wp('3'),
+    ...shadow(3),
+    alignItems: 'center',
+    marginTop: hp('2'),
+    paddingHorizontal: wp('4'),
+    alignSelf: 'center',
+  },
+  textInputStyle: {
+    width: wp('75'),
+    fontSize: wp('3.8'),
+  },
+  viewBottom: {
+    marginTop: hp('12'),
+    paddingHorizontal: wp('6'),
+  },
+  titleStyle: {
+    fontFamily: 'roboto-slab-bold',
+    fontSize: wp('6.5'),
+    color: '#ffffff',
+  },
+  content: {
+    fontFamily: 'roboto-slab-bold',
+    color: '#F7F3F3',
+    fontSize: wp('3.8'),
+  },
+  buttonStyle: {
+    width: wp('40'),
+    marginTop: wp('2'),
+  },
+  gradientOpacity: {
+    width: wp('100'),
+    height: hp('10'),
+    opacity: 0.2,
+  },
+});
+export default HomeSlideShow;
---
+++ src/components/SpotSlideShow.tsx
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import {View, StyleSheet, Text, ImageBackground} from 'react-native';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import {Pagination} from 'react-native-snap-carousel';
+import shadow from './shadow';
+import GradientOpacity from './GradientOpacity';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+class ProvincesSlideShow extends Component<any, any> {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const {data, dotsLength, activeDotIndex} = this.props;
+    return (
+      <ImageBackground
+        source={data.srcImage}
+        resizeMode={'cover'}
+        style={styles.MainContainer}>
+        <AntDesign
+          name={'close'}
+          size={wp('8')}
+          color={'#ffffff'}
+          style={{marginLeft: wp('4')}}
+        />
+        <View style={styles.viewPagination}>
+          <GradientOpacity styleGradient={styles.gradientOpacity} />
+          <Pagination
+            containerStyle={styles.paginationStyle}
+            dotsLength={dotsLength}
+            activeDotIndex={activeDotIndex}
+            dotStyle={styles.dotStyle}
+            inactiveDotStyle={styles.inactiveDotStyle}
+          />
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  MainContainer: {
+    paddingTop: hp('3'),
+    height: hp('30'),
+  },
+  viewPagination: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  paginationStyle: {
+    height: hp('7'),
+    paddingBottom: wp('2'),
+  },
+  dotStyle: {
+    width: wp('3'),
+    height: wp('3'),
+    borderRadius: wp('1.5'),
+    backgroundColor: 'rgba(255, 255, 255, 0.92)',
+    ...shadow(10),
+  },
+  inactiveDotStyle: {
+    width: wp('3'),
+    height: wp('3'),
+    borderRadius: wp('1.5'),
+    backgroundColor: 'rgba(255, 255, 255, 0.92)',
+    ...shadow(10),
+    opacity: 0.6,
+  },
+  gradientOpacity: {
+    width: wp('100'),
+    height: hp('10'),
+  },
+});
+export default ProvincesSlideShow;
