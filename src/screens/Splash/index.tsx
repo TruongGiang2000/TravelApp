dfdf64f2d6cc5e476d2e95e5cb2deda164c305b1
Giang <truonggiang20001807@gmail.com>
2020-11-25 23:16:34 +0700

    update

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -112,7 +112,7 @@ const SplashScreen = (props: any) => {
         break;
       case CodePush.SyncStatus.UP_TO_DATE:
       default:
-        setSyncMessage('ɚ 1.2');
+        setSyncMessage('ɚ 1.4');
         loadingApp();
         break;
     }
