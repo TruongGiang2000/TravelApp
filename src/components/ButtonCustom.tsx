3f6512e8f222a764bfa73c7da3932c202053dbb8
Giang <truonggiang20001807@gmail.com>
2020-12-12 09:57:15 +0700

    updaet

--- src/components/ButtonCustom.tsx
+++ src/components/ButtonCustom.tsx
@@ -1,33 +1,32 @@
 import React, {Component} from 'react';
-import {View, StyleSheet, Text, TouchableOpacity, Button} from 'react-native';
-import TraTe from './TraTe';
+import {StyleSheet, Pressable, Text} from 'react-native';
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
 class ButtonCustom extends Component<any, any> {
   constructor(props) {
     super(props);
-    this.state = {
-      
+    this.state = {};
   }
-}
 
   render() {
-     return (
-          <TouchableOpacity style={[styles.MainContainer, this.props.style]}>
-            <TraTe style={[styles.title,this.props.titleStyle]} i18nKey={this.props.title}/>
-          </TouchableOpacity>
-     )
+    const {title, style, titleStyle, onPress} = this.props;
+    return (
+      <Pressable style={[styles.MainContainer, style]} onPress={onPress}>
+        <Text style={[styles.title, titleStyle]}>{title}</Text>
+      </Pressable>
+    );
   }
 }
 const styles = StyleSheet.create({
   MainContainer: {
     backgroundColor: '#FA2A00',
-    borderRadius: 8,
-    paddingHorizontal: 14,
-    paddingVertical: 8,
+    borderRadius: wp('2'),
+    paddingHorizontal: wp('4'),
+    paddingVertical: wp('3'),
   },
   title: {
     color: '#fff',
-    fontWeight: 'bold',
     textAlign: 'center',
-  }
+    fontFamily: 'roboto-slab.regular',
+  },
 });
 export default ButtonCustom;
