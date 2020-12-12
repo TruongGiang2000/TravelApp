f8168f1931f42ff1e57da83301dd7c3bd027f37e
quang8122000 <bestxd2k@gmail.com>
2020-12-12 10:46:05 +0700

    update

--- src/components/ButtonCustom.tsx
+++ src/components/ButtonCustom.tsx
@@ -1,11 +1,7 @@
-import { title } from 'process';
+import {title} from 'process';
 import React, {Component} from 'react';
-<<<<<<< HEAD
-import {View, StyleSheet, Text, TouchableOpacity, Button} from 'react-native';
-=======
 import {StyleSheet, Pressable, Text} from 'react-native';
 import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
->>>>>>> 3f6512e8f222a764bfa73c7da3932c202053dbb8
 class ButtonCustom extends Component<any, any> {
   constructor(props) {
     super(props);
@@ -13,23 +9,12 @@ class ButtonCustom extends Component<any, any> {
   }
 
   render() {
-<<<<<<< HEAD
-    const {title} = this.props;
-     return (
-          <TouchableOpacity style={[styles.MainContainer, this.props.style]}>
-            <Text style={[styles.title,this.props.titleStyle]} >
-              {title}
-            </Text>
-          </TouchableOpacity>
-     )
-=======
     const {title, style, titleStyle, onPress} = this.props;
     return (
       <Pressable style={[styles.MainContainer, style]} onPress={onPress}>
         <Text style={[styles.title, titleStyle]}>{title}</Text>
       </Pressable>
     );
->>>>>>> 3f6512e8f222a764bfa73c7da3932c202053dbb8
   }
 }
 const styles = StyleSheet.create({
